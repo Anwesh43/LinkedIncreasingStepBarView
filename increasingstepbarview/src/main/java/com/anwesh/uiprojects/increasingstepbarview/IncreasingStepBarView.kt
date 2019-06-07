967ade48d6631bb076ef437f1d172b57927bee5b
Anwesh43 <anweshthecool0@gmail.com>
2019-06-07 11:13:34 +0530

    adding libraries and constants

--- increasingstepbarview/src/main/java/com/anwesh/uiprojects/increasingstepbarview/IncreasingStepBarView.kt
+++ increasingstepbarview/src/main/java/com/anwesh/uiprojects/increasingstepbarview/IncreasingStepBarView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.increasingstepbarview
 /**
  * Created by anweshmishra on 07/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
+val rects : Int = 5
+val scGap : Float = 0f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
