6d526a1192fcddbc8478ea7c74b7a1663ee3b4fd
Anwesh43 <anweshthecool0@gmail.com>
2019-06-07 11:34:15 +0530

    adding IncreasingStepBarView class

--- increasingstepbarview/src/main/java/com/anwesh/uiprojects/increasingstepbarview/IncreasingStepBarView.kt
+++ increasingstepbarview/src/main/java/com/anwesh/uiprojects/increasingstepbarview/IncreasingStepBarView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawISBNode(i : Int, scale : Float, paint : Paint) {
     restore()
     restore()
 }
+
+class IncreasingStepBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
