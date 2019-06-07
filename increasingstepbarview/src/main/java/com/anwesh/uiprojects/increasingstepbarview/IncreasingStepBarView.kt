89072f6efea9fbaa48a5bd22d6a7ecc673c030a5
Anwesh43 <anweshthecool0@gmail.com>
2019-06-07 11:47:59 +0530

    adding ISBNode class

--- increasingstepbarview/src/main/java/com/anwesh/uiprojects/increasingstepbarview/IncreasingStepBarView.kt
+++ increasingstepbarview/src/main/java/com/anwesh/uiprojects/increasingstepbarview/IncreasingStepBarView.kt
@@ -127,4 +127,48 @@ class IncreasingStepBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ISBNode(var i : Int, val state : State = State()) {
+
+        private var next : ISBNode? = null
+        private var prev : ISBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ISBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawISBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ISBNode {
+            var curr : ISBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
