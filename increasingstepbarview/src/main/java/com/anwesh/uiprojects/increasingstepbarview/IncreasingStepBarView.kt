5b5101db29c068d4df2b1447256ea9f7957c47b3
Anwesh43 <anweshthecool0@gmail.com>
2019-06-07 11:58:12 +0530

    using renderer in view

--- increasingstepbarview/src/main/java/com/anwesh/uiprojects/increasingstepbarview/IncreasingStepBarView.kt
+++ increasingstepbarview/src/main/java/com/anwesh/uiprojects/increasingstepbarview/IncreasingStepBarView.kt
@@ -67,14 +67,18 @@ fun Canvas.drawISBNode(i : Int, scale : Float, paint : Paint) {
 
 class IncreasingStepBarView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
